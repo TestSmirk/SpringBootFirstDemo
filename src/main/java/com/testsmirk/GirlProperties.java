e80456a664cf7ddc874046364888fc8afb2f618a
TestSmirk <d.kc@qq.com>
2017-01-27 13:11:54 +0800

    多种环境配置

---
+++ src/main/java/com/testsmirk/GirlProperties.java
@@ -0,0 +1,30 @@
+package com.testsmirk;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by testsmirk on 17-1-27.
+ */
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+    private String cupSize;
+    private Integer age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAgel() {
+        return age;
+    }
+
+    public void setAgel(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/testsmirk/HelloController.java
+++ src/main/java/com/testsmirk/HelloController.java
@@ -1,5 +1,8 @@
 package com.testsmirk;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Autowired
+    private GirlProperties girlProperties;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "Hello Spring Boot!";
+        return girlProperties.getCupSize();
     }
 }
