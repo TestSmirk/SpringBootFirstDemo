e9f256deff0baca936b01264615321a985ed62b4
TestSmirk <d.kc@qq.com>
2017-01-28 11:08:41 +0800

    多链接指向一个方法.

--- src/main/java/com/testsmirk/HelloController.java
+++ src/main/java/com/testsmirk/HelloController.java
@@ -3,8 +3,10 @@ package com.testsmirk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,7 +17,7 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello","/hi"}, method = RequestMethod.GET)
     public String say() {
         return girlProperties.getCupSize();
     }
