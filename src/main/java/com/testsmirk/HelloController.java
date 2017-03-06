fc73a8b87f4a523dcc776762e3e94cb65d86eb21
TestSmirk <d.kc@qq.com>
2017-03-06 13:32:44 +0800

    add query method

--- src/main/java/com/testsmirk/HelloController.java
+++ src/main/java/com/testsmirk/HelloController.java
@@ -1,12 +1,7 @@
 package com.testsmirk;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * Created by testsmirk on 17-1-27.
@@ -22,12 +17,22 @@ public class HelloController {
     public User say(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age) {
         System.out.println("HelloController.say " + name + "   age " + age);
         userRepository.save(new User(name, age));
-        return userRepository.findByUsername(name);
+        userRepository.findByUsername(name);
+        return userRepository.findByUserid(name);
     }
-    private String ObjId="ObjectId(\"%s\")";
+    //查询
+    @RequestMapping(value = "getuser", method = RequestMethod.GET)
+    public User getUser(@RequestParam(value = "id") String id) {
+        System.out.println(id);
+        return userRepository.findByUserid(id);
+
+    }
+
+    private String ObjId = "ObjectId(\"%s\")";
+
     @RequestMapping(value = {"del_hello"}, method = RequestMethod.DELETE)
-    public User delHello(@RequestParam(value = "id") String id){
-        String _id = String.format(ObjId,id);
+    public User delHello(@RequestParam(value = "id") String id) {
+        String _id = String.format(ObjId, id);
         System.out.print(_id);
         return userRepository.findAllExceptId(_id);
     }
--- src/main/java/com/testsmirk/UserRepository.java
+++ src/main/java/com/testsmirk/UserRepository.java
@@ -1,5 +1,6 @@
 package com.testsmirk;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,13 @@ import java.util.List;
  * Created by Smirk on 2017/3/4.
  */
 public interface UserRepository extends MongoRepository<User,Long>{
+    @Query("{'username':?0}")
     User findByUsername(String name);
+
 
     @Query("{'id':{'$ne':?0}}")
     User findAllExceptId(String id);
+
+    @Query("{'_id':?0}")
+    User findByUserid(String name);
 }
