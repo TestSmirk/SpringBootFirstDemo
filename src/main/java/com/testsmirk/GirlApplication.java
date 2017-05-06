1a146e4643fd77264e24190a14b7766752f96298
TestSmirk <d.kc@qq.com>
2017-05-06 23:01:03 +0800

    add database write in

--- src/main/java/com/testsmirk/GirlApplication.java
+++ src/main/java/com/testsmirk/GirlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GirlApplication {
 
 	public static void main(String[] args) {
+		//SpringBoot的启动初始化
 		SpringApplication.run(GirlApplication.class, args);
 	}
 }
